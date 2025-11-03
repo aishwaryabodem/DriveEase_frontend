dd266e40214fc8cfd7f1e11f9f49498b3c9927c8
aishwaryabodem <106659972+aishwaryabodem@users.noreply.github.com>
2025-11-03 22:22:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-var backendURL = "http://localhost:3000/api/cars";
-
 window.onload = function () {
   var carsList = document.getElementById("carsList");
-  var carSelect = document.getElementById("carSelect");
 
-  var request = new XMLHttpRequest();
-  request.open("GET", backendURL, true);
+  var cars = [
+    { name: "Maruti Swift", price: 1500, image: "swift.jpg" },
+    { name: "Hyundai i20", price: 1800, image: "i20.jpg" },
+    { name: "Tata Nexon", price: 2500, image: "nexon.jpg" },
+    { name: "Toyota Innova", price: 3000, image: "innova.jpg" },
+    { name: "Honda City", price: 2800, image: "city.jpg" }
+  ];
 
-  request.onload = function () {
-    if (request.status === 200) {
-      var cars = JSON.parse(request.responseText);
-
-      if (carsList) {
-        carsList.innerHTML = "";
-        for (var i = 0; i < cars.length; i++) {
-          var car = cars[i];
-          var card = document.createElement("div");
-          card.className = "car";
-          card.innerHTML =
-            "<img src='images/" + car.image + "' width='200'><br>" +
-            "<b>" + car.name + "</b><br>" +
-            "Price: ₹" + car.price + "/day<br><br>";
-          carsList.appendChild(card);
-        }
-      }
-
-      if (carSelect) {
-        carSelect.innerHTML = "";
-        for (var j = 0; j < cars.length; j++) {
-          var option = document.createElement("option");
-          option.value = cars[j].name;
-          option.textContent = cars[j].name;
-          carSelect.appendChild(option);
-        }
-      }
-    } else {
-      if (carsList) carsList.innerHTML = "Failed to load car data.";
-      if (carSelect) {
-        var option = document.createElement("option");
-        option.textContent = "Error loading cars";
-        carSelect.appendChild(option);
-      }
-    }
-  };
-
-  request.send();
+  carsList.innerHTML = "";
+  for (var i = 0; i < cars.length; i++) {
+    var car = cars[i];
+    var card = document.createElement("div");
+    card.className = "car";
+    card.innerHTML =
+      "<img src='images/" + car.image + "' width='200'><br>" +
+      "<b>" + car.name + "</b><br>" +
+      "Price: ₹" + car.price + "/day<br><br>";
+    carsList.appendChild(card);
+  }
 };

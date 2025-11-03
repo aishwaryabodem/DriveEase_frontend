178e60927f070596baa32142de37c8301290029b
Keerthi Bodem <aishwaryabodem@gmail.com>
2025-11-03 22:45:00 +0530

    Updated cars page to show car list without backend

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
+// If backend not available, use this fallback data
+var cars = [
+  { name: "Maruti Suzuki Swift", price: 1500, image: "swift.jpg" },
+  { name: "Tata Nexon", price: 2000, image: "nexon.jpg" },
+  { name: "Hyundai Creta", price: 2500, image: "creta.jpg" },
+  { name: "Mahindra XUV700", price: 3000, image: "xuv700.jpg" },
+  { name: "Kia Seltos", price: 2700, image: "seltos.jpg" },
+  { name: "Maruti Baleno", price: 2200, image: "baleno.jpg" },
+  { name: "Toyota Innova Crysta", price: 3200, image: "innova.jpg" },
+  { name: "MG Hector", price: 2800, image: "hector.jpg" }
+];
+
 window.onload = function () {
   var carsList = document.getElementById("carsList");
+  carsList.innerHTML = "";
 
-  // Your real car data (frontend only)
-  var cars = [
-    { name: "Maruti Suzuki Swift", price: 1500, image: "images/swift.jpg" },
-    { name: "Tata Nexon", price: 2000, image: "images/nexon.jpg" },
-    { name: "Hyundai Creta", price: 2500, image: "images/creta.jpg" },
-    { name: "Mahindra XUV700", price: 3000, image: "images/xuv700.jpg" },
-    { name: "Kia Seltos", price: 2700, image: "images/seltos.jpg" },
-    { name: "Maruti Baleno", price: 2200, image: "images/Baleno.jpg" },
-    { name: "Toyota Innova Crysta", price: 3200, image: "images/innova.jpg" },
-    { name: "MG Hector", price: 2800, image: "images/hector.jpg" }
-  ];
-
-  if (carsList) {
-    carsList.innerHTML = "";
-    for (var i = 0; i < cars.length; i++) {
-      var car = cars[i];
-      var card = document.createElement("div");
-      card.className = "car";
-      card.innerHTML =
-        "<img src='" + car.image + "' width='200'><br>" +
-        "<b>" + car.name + "</b><br>" +
-        "Price: ₹" + car.price + "/day<br><br>";
-      carsList.appendChild(card);
-    }
+  for (var i = 0; i < cars.length; i++) {
+    var car = cars[i];
+    var card = document.createElement("div");
+    card.className = "car";
+    card.innerHTML =
+      "<img src='images/" + car.image + "' width='200'><br>" +
+      "<b>" + car.name + "</b><br>" +
+      "Price: ₹" + car.price + "/day<br><br>";
+    carsList.appendChild(card);
   }
 };

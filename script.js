45cba76b9bd94a03fc02f53f736f97efc9cffb0e
Keerthi Bodem <aishwaryabodem@gmail.com>
2025-11-03 23:01:12 +0530

    Fixed booking dropdown to show all cars

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-// If backend not available, use this fallback data
-var cars = [
+const cars = [
   { name: "Maruti Suzuki Swift", price: 1500, image: "swift.jpg" },
   { name: "Tata Nexon", price: 2000, image: "nexon.jpg" },
   { name: "Hyundai Creta", price: 2500, image: "creta.jpg" },
   { name: "Mahindra XUV700", price: 3000, image: "xuv700.jpg" },
   { name: "Kia Seltos", price: 2700, image: "seltos.jpg" },
- { name: "Maruti Baleno", price: 2200, image: "baleno_new.jpg" },
+  { name: "Maruti Baleno", price: 2200, image: "baleno_new.jpg" },
   { name: "Toyota Innova Crysta", price: 3200, image: "innova.jpg" },
   { name: "MG Hector", price: 2800, image: "hector.jpg" }
 ];
 
-window.onload = function () {
-  var carsList = document.getElementById("carsList");
-  carsList.innerHTML = "";
+const carsList = document.getElementById("carsList");
+if (carsList) {
+  cars.forEach(car => {
+    const div = document.createElement("div");
+    div.className = "car-card";
+    div.innerHTML = `
+      <img src="${car.image}" alt="${car.name}">
+      <h3>${car.name}</h3>
+      <p>Price: ₹${car.price}/day</p>
+    `;
+    carsList.appendChild(div);
+  });
+}
 
-  for (var i = 0; i < cars.length; i++) {
-    var car = cars[i];
-    var card = document.createElement("div");
-    card.className = "car";
-    card.innerHTML =
-      "<img src='images/" + car.image + "' width='200'><br>" +
-      "<b>" + car.name + "</b><br>" +
-      "Price: ₹" + car.price + "/day<br><br>";
-    carsList.appendChild(card);
-  }
-};
+const carSelect = document.getElementById("carSelect");
+if (carSelect) {
+  cars.forEach(car => {
+    const option = document.createElement("option");
+    option.value = car.name;
+    option.textContent = car.name;
+    carSelect.appendChild(option);
+  });
+}
